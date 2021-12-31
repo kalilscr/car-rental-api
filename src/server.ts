a0c8ec47cdfbebe12aedf86f6d97d2a168b5143f
rodrigo kalil <rkbuarque@gmail.com>
2021-12-30 23:44:42 -0300

    configurando debugger

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,14 @@ import { createCourse } from './routes';
 
 const app = express();
 
+app.use(express.json());
+
 app.get("/", createCourse);
+
+app.post("/courses", (request, response) => {
+    const { name } = request.body;
+
+    return response.json({ name });
+});
 
 app.listen(3333, () => console.log("Server is running!!"));
