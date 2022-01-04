23bf27612643b2731821a449b4b135c2c708dbc9
rodrigo kalil <rkbuarque@gmail.com>
2022-01-04 08:31:14 -0300

    Utilizando o Princípio da Substituição de Liskov (LSP) SOLID

--- src/repositories/CategoriesRepository.ts
+++ src/repositories/CategoriesRepository.ts
@@ -1,11 +1,7 @@
 import { Category } from "../model/Category";
+import { ICategoriesRepository, ICreateCategoryDTO } from "./ICategoriesRepository";
 
-interface ICreateCategoryDTO {
-    name: string;
-    description: string;
-}
-
-class CategoriesRepository {
+class CategoriesRepository implements ICategoriesRepository {
 
     private categories: Category[];
 
@@ -35,4 +31,4 @@ class CategoriesRepository {
     }
 }
 
-export { CategoriesRepository };
+export { CategoriesRepository, ICategoriesRepository };
