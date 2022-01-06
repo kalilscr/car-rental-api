574263881a85ad85fb2fce4600bb9c109326545b
rodrigo kalil <rkbuarque@gmail.com>
2022-01-06 07:43:45 -0300

    Criando Use Cases de especificação

--- src/modules/cars/repositories/implementations/SpecificationsRepository.ts
+++ src/modules/cars/repositories/implementations/SpecificationsRepository.ts
@@ -5,8 +5,18 @@ class SpecificationsRepository implements ISpecificationsRepository {
 
     private specifications: Specification[];
 
+    private static INSTANCE: SpecificationsRepository;
+
     constructor() {
         this.specifications = [];
+    }
+
+    public static getInstance(): SpecificationsRepository {
+        if(!SpecificationsRepository.INSTANCE) {
+            SpecificationsRepository.INSTANCE = new SpecificationsRepository();
+        }
+
+        return SpecificationsRepository.INSTANCE;
     }
 
     create({ description, name } : ICreateSpecificationDTO): void {
--- src/routes/specifications.routes.ts
+++ src/routes/specifications.routes.ts
@@ -1,25 +1,16 @@
 import { Router } from "express";
-import { SpecificationsRepository } from "../modules/cars/repositories/implementations/SpecificationsRepository";
-import { CreateSpecificationService } from "../modules/cars/services/CreateSpecificationService";
+import { createSpecificationController } from "../modules/cars/useCases/createSpecification";
+import { listSpecificationsController } from "../modules/cars/useCases/listSpecifications";
 
 const specificationsRoutes = Router();
-const specificationsRepository = new SpecificationsRepository();
 
 specificationsRoutes.post("/", (request, response) => {
-    const { name, description } = request.body;
-
-    const createSpecificationService = new CreateSpecificationService(specificationsRepository)
-    
-    createSpecificationService.execute({ name, description });
-
-    return response.status(201).send();
+    return createSpecificationController.handle(request, response);
 });
 
 
 specificationsRoutes.get("/", (request, response) => {
-    const allSpecifications = specificationsRepository.list();
-
-    return response.json(allSpecifications);
+    return listSpecificationsController.handle(request, response);
 });
 
 export { specificationsRoutes };
