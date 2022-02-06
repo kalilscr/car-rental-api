8a412f2a9b99424ba689dccf9929bfaf13c18b28
Rodrigo Kalil <rkbuarque@gmail.com>
2022-02-05 22:28:43 -0300

    criando config de upload e upload de avatar

--- src/modules/accounts/useCases/updateUserAvatar/updateUserAvatarController.ts
+++ src/modules/accounts/useCases/updateUserAvatar/updateUserAvatarController.ts
@@ -5,8 +5,7 @@ import { UpdateUserAvatarUseCase } from "./updateUserAvatarUseCase";
 class UpdateUserAvatarController {
     async handle(request: Request, response: Response): Promise<Response> {
         const { id } = request.user;
-
-        const avatar_file = null;
+        const avatar_file = request.file.filename;
 
         const updateUserAvatarUseCase = container.resolve(
             UpdateUserAvatarUseCase
--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,15 +1,15 @@
 import { Router } from "express";
 import multer from "multer";
 
+import uploadConfig from "../config/upload";
+import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
 import { CreateUserController } from "../modules/accounts/useCases/createUser/CreateUserController";
 import { UpdateUserAvatarController } from "../modules/accounts/useCases/updateUserAvatar/updateUserAvatarController";
 
 const usersRoutes = Router();
 
-const upload = multer({
-    dest: "./avatar",
-});
-
+const uploadAvatar = multer(uploadConfig.upload("./temp/avatar"));
+ 
 const createUserController = new CreateUserController();
 const updateUserAvatarController = new UpdateUserAvatarController();
 
@@ -17,7 +17,8 @@ usersRoutes.post("/", createUserController.handle);
 
 usersRoutes.patch(
     "/avatar", 
-    upload.single("file"), 
+    ensureAuthenticated,
+    uploadAvatar.single("avatar"), 
     updateUserAvatarController.handle
 );
 
