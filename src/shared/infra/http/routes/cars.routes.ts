a57ba4ab7b36dbfa834272e4a2f2e018c9971313
Rodrigo Kalil <rkbuarque@gmail.com>
2022-02-17 16:23:16 -0300

    Criando middleware de administrador e adicionando em rotas

--- src/shared/infra/http/routes/cars.routes.ts
+++ src/shared/infra/http/routes/cars.routes.ts
@@ -1,5 +1,9 @@
 import { Router } from "express";
+
 import { CreateCarController } from "../../../../modules/cars/useCases/createCar/CreateCarController";
+
+import { ensureAdmin } from "../middlewares/ensureAdmin";
+import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
 
 const carsRoutes = Router();
 
@@ -7,6 +11,8 @@ const createCarController = new CreateCarController();
 
 carsRoutes.post(
   "/",
+  ensureAuthenticated,
+  ensureAdmin,
   createCarController.handle
 );
 
--- src/shared/infra/http/routes/specifications.routes.ts
+++ src/shared/infra/http/routes/specifications.routes.ts
@@ -1,16 +1,25 @@
 import { Router } from "express";
-import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
 
 import { CreateSpecificationController } from "../../../../modules/cars/useCases/createSpecification/CreateSpecificationController";
 import { ListSpecificationsController } from "../../../../modules/cars/useCases/listSpecifications/ListSpecificationsController";
+
+import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
+import { ensureAdmin } from "../middlewares/ensureAdmin";
 
 const specificationsRoutes = Router();
 
 const createSpecificationController = new CreateSpecificationController();
 const listSpecificationsController = new ListSpecificationsController();
 
-specificationsRoutes.use(ensureAuthenticated);
-specificationsRoutes.post("/", createSpecificationController.handle);
+//specificationsRoutes.use(ensureAuthenticated); // assim todas as rotas abaixo estarão protegidas(estarão passando pelo middleware)
+// rotas que usam o middleware pra baixo e as que não precisam de middleware pra cima do specificationsRoutes.use()
+specificationsRoutes.post(
+    "/", 
+    ensureAuthenticated,
+    ensureAdmin,
+    createSpecificationController.handle
+    );
+
 specificationsRoutes.get("/", listSpecificationsController.handle);
 
 export { specificationsRoutes };
