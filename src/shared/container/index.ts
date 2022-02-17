c479ed7582b7f22e0d9976f5813d11298393cc65
Rodrigo Kalil <rkbuarque@gmail.com>
2022-02-16 21:57:59 -0300

    Estruturando a entidade de carros, adicionando cars no container, criando CarsRepository, CreateCarsController e rota

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -6,6 +6,8 @@ import { CategoriesRepository } from "../../modules/cars/infra/typeorm/repositor
 import { ICategoriesRepository } from "../../modules/cars/repositories/ICategoriesRepository";
 import { SpecificationsRepository } from "../../modules/cars/infra/typeorm/repositories/SpecificationsRepository";
 import { ISpecificationsRepository } from "../../modules/cars/repositories/ISpecificationsRepository";
+import { CarsRepository } from "../../modules/cars/infra/typeorm/repositories/CarsRepository";
+import { ICarsRepository } from "../../modules/cars/repositories/ICarsRepository";
 
 container.registerSingleton<ICategoriesRepository>(
     "CategoriesRepository",
@@ -20,4 +22,9 @@ container.registerSingleton<ISpecificationsRepository>(
 container.registerSingleton<IUsersRepository>(
     "UsersRepository",
     UsersRepository
+);
+
+container.registerSingleton<ICarsRepository>(
+    "CarsRepository",
+    CarsRepository
 );
