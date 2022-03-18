348d31cac9d20e67dc541fa98784e12eab4133e5
Rodrigo Kalil <rkbuarque@gmail.com>
2022-03-18 14:40:08 -0300

    creating rentals migrations

--- src/shared/infra/typeorm/migrations/1643825877420-CreateUsers.ts
+++ src/shared/infra/typeorm/migrations/1643825877420-CreateUsers.ts
@@ -9,6 +9,7 @@ export class CreateUsers1643825877420 implements MigrationInterface {
                     {
                         name: "id",
                         type: "uuid",
+                        isPrimary: true,
                     },
                     {
                         name: "name",
