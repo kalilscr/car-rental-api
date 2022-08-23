6fe3320bc593a16d66954c2508d2c69d4c1ef5b5
Rodrigo Kalil <rodrigo.kalil@rarolabs.com.br>
2022-08-23 07:36:32 -0300

    corrigindo status de um carro

--- src/modules/cars/infra/typeorm/repositories/CarsRepository.ts
+++ src/modules/cars/infra/typeorm/repositories/CarsRepository.ts
@@ -6,79 +6,88 @@ import { ICarsRepository } from "../../../repositories/ICarsRepository";
 import { Car } from "../entities/Car";
 
 class CarsRepository implements ICarsRepository {
-  private repository: Repository<Car>;
+    private repository: Repository<Car>;
 
-  constructor() {
-    this.repository = getRepository(Car);
-  }
-    
-  async create({
-    brand,
-    category_id,
-    daily_rate,
-    description,
-    fine_amount,
-    license_plate,
-    name,
-    specifications,
-    id,
-  }: ICreateCarDTO): Promise<Car> {
-    const car = this.repository.create({
-      brand,
-      category_id,
-      daily_rate,
-      description,
-      fine_amount,
-      license_plate,
-      name,
-      specifications,
-      id,
-    });
-
-    await this.repository.save(car);
-
-    return car;
-  }
-
-  async findByLicensePlate(license_plate: string): Promise<Car> {
-    const car = await this.repository.findOne({
-      license_plate,
-    });
-
-    return car;
-  }
-
-  async findAllAvailable(
-    brand?: string,
-    category_id?: string,
-    name?: string
-  ): Promise<Car[]> {
-    const carsQuery = await this.repository
-      .createQueryBuilder("c") // alias
-      .where("available = :available", { available: true }); //available = nome do parâmetro que esta dentro da tabela //:available = valor do parâmetro que a gente espera que seja passado, no caso true
-
-    if (brand) {
-      carsQuery.andWhere("brand = :brand", { brand });
+    constructor() {
+        this.repository = getRepository(Car);
     }
 
-    if (name) {
-      carsQuery.andWhere("name = :name", { name });
+    async create({
+        brand,
+        category_id,
+        daily_rate,
+        description,
+        fine_amount,
+        license_plate,
+        name,
+        specifications,
+        id,
+    }: ICreateCarDTO): Promise<Car> {
+        const car = this.repository.create({
+            brand,
+            category_id,
+            daily_rate,
+            description,
+            fine_amount,
+            license_plate,
+            name,
+            specifications,
+            id,
+        });
+
+        await this.repository.save(car);
+
+        return car;
     }
 
-    if (category_id) {
-      carsQuery.andWhere("category_id = :category_id", { category_id });
+    async findByLicensePlate(license_plate: string): Promise<Car> {
+        const car = await this.repository.findOne({
+            license_plate,
+        });
+
+        return car;
     }
 
-    const cars = await carsQuery.getMany();
+    async findAllAvailable(
+        brand?: string,
+        category_id?: string,
+        name?: string
+    ): Promise<Car[]> {
+        const carsQuery = await this.repository
+            .createQueryBuilder("c") // alias
+            .where("available = :available", { available: true }); //available = nome do parâmetro que esta dentro da tabela //:available = valor do parâmetro que a gente espera que seja passado, no caso true
 
-    return cars;
-  }
+        if (brand) {
+            carsQuery.andWhere("brand = :brand", { brand });
+        }
 
-  async findById(id: string): Promise<Car> {
-    const car = await this.repository.findOne(id);
-    return car;
-  }
+        if (name) {
+            carsQuery.andWhere("name = :name", { name });
+        }
 
+        if (category_id) {
+            carsQuery.andWhere("category_id = :category_id", { category_id });
+        }
+
+        const cars = await carsQuery.getMany();
+
+        return cars;
+    }
+
+    async findById(id: string): Promise<Car> {
+        const car = await this.repository.findOne(id);
+        return car;
+    }
+
+    async updateAvailable(id: string, available: boolean): Promise<void> {
+        await this.repository
+            .createQueryBuilder()
+            .update()
+            .set({ available })
+            .where("id = :id")
+            .setParameters({ id })
+            .execute();
+    }
 }
 
 export { CarsRepository };
--- src/modules/rentals/useCases/createRental/CreateRentalUseCase.ts
+++ src/modules/rentals/useCases/createRental/CreateRentalUseCase.ts
@@ -7,66 +7,65 @@ import { Rental } from "../../infra/typeorm/entities/Rental";
 import { IRentalsRepository } from "../../repositories/IRentalsRepository";
 
 interface IRequest {
-  user_id: string;
-  car_id: string;
-  expected_return_date: Date;
+    user_id: string;
+    car_id: string;
+    expected_return_date: Date;
 }
 
 @injectable()
 class CreateRentalUseCase {
-  constructor(
-    @inject("RentalsRepository")
-    private rentalsRepository: IRentalsRepository,
-    @inject("DayjsDateProvider")
-    private dateProvider: IDateProvider,
-    /* @inject("CarsRepository")
-    private carsRepository: ICarsRepository */
-  ) {}
+    constructor(
+        @inject("RentalsRepository")
+        private rentalsRepository: IRentalsRepository,
+        @inject("DayjsDateProvider")
+        private dateProvider: IDateProvider,
+        @inject("CarsRepository")
+        private carsRepository: ICarsRepository
+    ) {}
 
-  async execute({
-    user_id,
-    car_id,
-    expected_return_date,
-  }: IRequest): Promise<Rental> {
-    const minimumHour = 24;
+    async execute({
+        user_id,
+        car_id,
+        expected_return_date,
+    }: IRequest): Promise<Rental> {
+        const minimumHour = 24;
 
-    const carUnavailable = await this.rentalsRepository.findOpenRentalByCar(
-      car_id
-    );
+        const carUnavailable = await this.rentalsRepository.findOpenRentalByCar(
+            car_id
+        );
 
-    if (carUnavailable) {
-      throw new AppError("Car is unavailable");
+        if (carUnavailable) {
+            throw new AppError("Car is unavailable");
+        }
+
+        const rentalOpenToUser =
+            await this.rentalsRepository.findOpenRentalByUser(user_id);
+
+        if (rentalOpenToUser) {
+            throw new AppError("There's a rental in progress for this user!");
+        }
+
+        const dateNow = this.dateProvider.dateNow();
+
+        const compare = this.dateProvider.compareInHours(
+            dateNow,
+            expected_return_date
+        );
+
+        if (compare < minimumHour) {
+            throw new AppError("Invalid return time!");
+        }
+
+        const rental = await this.rentalsRepository.create({
+            user_id,
+            car_id,
+            expected_return_date,
+        });
+
+        await this.carsRepository.updateAvailable(car_id, false);
+
+        return rental;
     }
-
-    const rentalOpenToUser = await this.rentalsRepository.findOpenRentalByUser(
-      user_id
-    );
-
-    if (rentalOpenToUser) {
-      throw new AppError("There's a rental in progress for this user!");
-    }
-
-    const dateNow = this.dateProvider.dateNow();
-
-    const compare = this.dateProvider.compareInHours(
-      dateNow,
-      expected_return_date
-    );
-
-    if (compare < minimumHour) {
-      throw new AppError("Invalid return time!");
-    }
-
-    const rental = await this.rentalsRepository.create({
-      user_id,
-      car_id,
-      expected_return_date,
-    });
-
-    //await this.carsRepository.updateAvailable(car_id, false);
-
-    return rental;
-  }
 }
 
 export { CreateRentalUseCase };
