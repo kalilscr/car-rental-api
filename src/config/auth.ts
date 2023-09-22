3b642ca05e1ba5c982feb3d667b9ee09ac052864
Rodrigo Kalil <rkbuarque@gmail.com>
2023-09-22 17:20:34 -0300

    refact: adds refresh token on user authentication

---
+++ src/config/auth.ts
@@ -0,0 +1,7 @@
+export default {
+    secret_token: "cfe275a5908b5650488e0b0342c2d6cc",
+    expires_in_token: "15m",
+    secret_refresh_token: "d12f8e21c5317578000531a505c2993c",
+    expires_in_refresh_token: "30d",
+    expires_refresh_token_days: 30,
+  };
--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -14,6 +14,8 @@ import { CarsImagesRepository } from "../../modules/cars/infra/typeorm/repositor
 import { ICarsImagesRepository } from "../../modules/cars/repositories/ICarsImagesRepository";
 import { IRentalsRepository } from "../../modules/rentals/repositories/IRentalsRepository";
 import { RentalsRepository } from "../../modules/rentals/infra/typeorm/repositories/RentalsRepository";
+import { IUsersTokensRepository } from "../../modules/accounts/repositories/IUsersTokensRepository";
+import { UsersTokensRepository } from "../../modules/accounts/infra/typeorm/repositories/UsersTokensRepository";
 
 container.registerSingleton<ICategoriesRepository>(
     "CategoriesRepository",
@@ -44,3 +46,8 @@ container.registerSingleton<IRentalsRepository>(
     "RentalsRepository",
     RentalsRepository
 );
+
+container.registerSingleton<IUsersTokensRepository>(
+    "UsersTokensRepository",
+    UsersTokensRepository
+  );
