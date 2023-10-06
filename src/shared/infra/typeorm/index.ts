61825dcc5ad00b1aba54f5f7bc4102024e5665dd
Rodrigo Kalil <rkbuarque@gmail.com>
2023-10-06 17:31:55 -0300

    chore: change orm connection config prod

--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -1,15 +1,28 @@
-import { Connection, createConnection, getConnectionOptions } from "typeorm";
+import { Connection, createConnection, getConnectionOptions } from 'typeorm';
 
-export default async (host = "database"): Promise<Connection> => {
+// export default async (host = "database"): Promise<Connection> => {
+//     const defaultOptions = await getConnectionOptions();
+
+//     return createConnection(
+//         Object.assign(defaultOptions, {
+//             host: process.env.NODE_ENV === "test" ? "localhost" : host, // Essa opção deverá ser EXATAMENTE o nome dado ao service do banco de dados
+//             database:
+//             process.env.NODE_ENV === "test"
+//             ? "carrentalx"
+//             : defaultOptions.database,
+//         })
+//     );
+// };
+
+export default async (): Promise<Connection> => {
     const defaultOptions = await getConnectionOptions();
 
     return createConnection(
         Object.assign(defaultOptions, {
-            host: process.env.NODE_ENV === "test" ? "localhost" : host, // Essa opção deverá ser EXATAMENTE o nome dado ao service do banco de dados
-            database: 
-            process.env.NODE_ENV === "test" 
-            ? "carrentalx" 
-            : defaultOptions.database,
-        })
+            database:
+                process.env.NODE_ENV === 'test'
+                    ? 'carrentalx'
+                    : defaultOptions.database,
+        }),
     );
 };
