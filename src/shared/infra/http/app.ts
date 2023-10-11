dbe91061e3ec2f52f9c2fc41ed6e352fc997acb5
Rodrigo Kalil <rkbuarque@gmail.com>
2023-10-11 15:59:35 -0300

    feat: adjusts sentry

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -18,54 +18,66 @@ import { router } from './routes';
 
 createConnection();
 const app = express();
+try {
+    app.use(rateLimiter);
 
-app.use(rateLimiter);
+    Sentry.init({
+        dsn: process.env.SENTRY_DSN,
+        includeLocalVariables: true,
+        integrations: [
+            new Sentry.Integrations.LocalVariables({
+                captureAllExceptions: true,
+            }),
+            new Sentry.Integrations.Http({ tracing: true }),
+            new Sentry.Integrations.Express({ app }),
+            new ProfilingIntegration(),
+        ],
+        // Performance Monitoring
+        tracesSampleRate: 1.0, // Capture 100% of the transactions, reduce in production!
+        // Set sampling rate for profiling - this is relative to tracesSampleRate
+        profilesSampleRate: 1.0, // Capture 100% of the transactions, reduce in production!
+    });
 
-Sentry.init({
-    dsn: process.env.SENTRY_DSN,
-    integrations: [
-        new Sentry.Integrations.Http({ tracing: true }),
-        new Sentry.Integrations.Express({ app }),
-        new ProfilingIntegration(),
-    ],
-    // Performance Monitoring
-    tracesSampleRate: 1.0, // Capture 100% of the transactions, reduce in production!
-    // Set sampling rate for profiling - this is relative to tracesSampleRate
-    profilesSampleRate: 1.0, // Capture 100% of the transactions, reduce in production!
-});
+    app.use(Sentry.Handlers.requestHandler());
+    app.use(Sentry.Handlers.tracingHandler());
 
-app.use(Sentry.Handlers.requestHandler());
-app.use(Sentry.Handlers.tracingHandler());
+    app.use(express.json());
 
-app.use(express.json());
+    app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerFile));
 
-app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerFile));
+    app.use('/avatar', express.static(`${upload.tmpFolder}/avatar`));
+    app.use('/cars', express.static(`${upload.tmpFolder}/cars`));
 
-app.use('/avatar', express.static(`${upload.tmpFolder}/avatar`));
-app.use('/cars', express.static(`${upload.tmpFolder}/cars`));
+    app.use(
+        cors({
+            origin: '*',
+        }),
+    );
+    app.use(router);
 
-app.use(
-    cors({
-        origin: '*',
-    }),
-);
-app.use(router);
+    app.use(Sentry.Handlers.errorHandler());
 
-app.use(Sentry.Handlers.errorHandler());
+    app.use(
+        (
+            err: Error,
+            request: Request,
+            response: Response,
+            next: NextFunction,
+        ) => {
+            if (err instanceof AppError) {
+                return response.status(err.statusCode).json({
+                    message: err.message,
+                });
+            }
 
-app.use(
-    (err: Error, request: Request, response: Response, next: NextFunction) => {
-        if (err instanceof AppError) {
-            return response.status(err.statusCode).json({
-                message: err.message,
+            return response.status(500).json({
+                status: 'error',
+                message: `Internal server error - ${err.message}`,
             });
-        }
-
-        return response.status(500).json({
-            status: 'error',
-            message: `Internal server error - ${err.message}`,
-        });
-    },
-);
+        },
+    );
+} catch (e) {
+    Sentry.captureException(e);
+}
 
 export { app };
